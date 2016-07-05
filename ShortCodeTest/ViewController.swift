8f9ab70b016ac1c1854804a833c1c59ea2cc05b8
mallesh0301 <mallesh@nareshs-macbook-pro.local>
2016-07-05 17:19:15 -0500

    Initial Commit

---
+++ ShortCodeTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShortCodeTest
+//
+//  Created by mallesh on 7/5/16.
+//  Copyright © 2016 mallesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
