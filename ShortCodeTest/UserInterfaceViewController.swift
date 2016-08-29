9bcec9b9bc15626224369b8f3c9de99d777d4bf2
mallesh0301 <mallesh@nareshs-macbook-pro.local>
2016-08-29 01:15:36 -0500

    Modifications

---
+++ ShortCodeTest/UserInterfaceViewController.swift
@@ -0,0 +1,47 @@
+//
+//  UserInterfaceViewController.swift
+//  ShortCodeTest
+//
+//  Created by mallesh on 8/29/16.
+//  Copyright © 2016 mallesh. All rights reserved.
+//
+
+import UIKit
+
+class UserInterfaceViewController: UIViewController {
+
+    @IBOutlet weak var inputField: UITextField!
+    
+    var input:Int? = Int()
+    
+    @IBAction func showResults(sender: AnyObject) {
+        
+        guard let text = self.inputField.text where !text.isEmpty else {
+            let alert = UIAlertController(title: "Invalid Data", message: "Please enter a number from 1 to 1000", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+            return
+        }
+        
+        input = Int(self.inputField.text!)
+        
+        if input > 1000 {
+            let alert = UIAlertController(title: "Invalid Data", message: "Please enter a value less than 1000", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+        else {
+            self.performSegueWithIdentifier("SHOW_TABLE", sender: sender)
+        }
+        
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if(segue.identifier == "SHOW_TABLE") {
+            let getData: ViewController = segue.destinationViewController as! ViewController
+            getData.num = input!
+        }
+    }
+
+    
+}
--- ShortCodeTest/ViewController.swift
+++ ShortCodeTest/ViewController.swift
@@ -12,6 +12,10 @@ import Foundation
 
 class ViewController: UIViewController, NSFetchedResultsControllerDelegate, UITableViewDataSource, UITableViewDelegate {
     
+    var num : Int? = Int()
+    
+    var api : GetJSONData = GetJSONData()
+    
     // Outlet for the tableview
     @IBOutlet weak var tableView: UITableView!
     
@@ -21,55 +25,8 @@ class ViewController: UIViewController, NSFetchedResultsControllerDelegate, UITa
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Store the json URL
-        let url = NSURL(string: "http://www.filltext.com/?rows=100&fname=%7BfirstName%7D&lname=%7BlastName%7D&city=%7Bcity%7D&pretty=true")!
+        api.readJSONData(appdel, num : num!)
         
-        // Access the url using the NSURLSession
-        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) in
-            if error != nil {
-                print("there is some problem with data\n")
-            }
-            else {
-                if let urlContent = data {
-                    do {
-                        // getting the data from the url fjg
-                        let jsonArr = try NSJSONSerialization.JSONObjectWithData(urlContent, options: NSJSONReadingOptions.MutableContainers) as! NSArray
-                        
-                        // if the url is not empty
-                        if jsonArr.count > 0 {
-                            for i in 0..<jsonArr.count {
-
-                                // get the array data into the dictionaries
-                                if let jsonDic = jsonArr.objectAtIndex(i) as? NSDictionary {
-                                    
-                                    // check if json is empty
-                                    guard   let fname = jsonDic["fname"],
-                                            let lname = jsonDic["lname"],
-                                            let city = jsonDic["city"]
-                                    else {
-                                        print("json data is empty!")
-                                        return
-                                    }
-                                    
-                                    // create an entity object to store the data into tables
-                                    let newPost = NSEntityDescription.insertNewObjectForEntityForName("Details", inManagedObjectContext: self.appdel.managedObjectContext)
-                                    
-                                    // add the values to the attributes
-                                    
-                                    newPost.setValue(fname, forKey: "fname")
-                                    newPost.setValue(lname, forKey: "lname")
-                                    newPost.setValue(city, forKey: "city")
-                                }
-                            }
-                        }
-                    }
-                    catch {}
-                }
-            }
-        }
-        
-        // perform the NSURLSession task
-        task.resume()
     }
     
     override func didReceiveMemoryWarning() {
